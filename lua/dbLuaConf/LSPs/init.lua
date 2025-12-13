29e3adc8670596bfaf1f17d5f85d358f1f31abbd
hyeondobin <dobinhyeon@gmail.com>
2025-12-13 20:49:25 +0900

    add LSP configs

---
+++ lua/dbLuaConf/LSPs/init.lua
@@ -0,0 +1,93 @@
+local catUtils = require("nixCatsUtils")
+
+require('lze').h.lsp.set_ft_fallback(function(name)
+    local lspcfg = nixCats.pawsible({ "allPlugins", "opt", "nvim-lspconfig" }) or nixCats.pawsible({ "allPlugins", "start", "nvim-lspconfig" })
+    if lspcfg then
+        local ok, cfg = pcall(dofile, lspcfg .. "/lsp/" .. name .. ".lua")
+        if not ok then
+            ok, cfg = pcall(dofile, lspcfg .. "/lua/lspconfig/configs/" .. name .. ".lua")
+        end
+        return (ok and cfg or {}).filetypes or {}
+    else
+        return old_ft_fallback(name)
+    end
+end)
+require("lze").load {
+    {
+        "nvim-lspconfig",
+        on_require = { "lspconfig" },
+        lsp = function(plugin)
+            vim.lsp.config(plugin.name, plugin.lsp or {})
+            vim.lsp.enable(plugin.name)
+        end,
+        before = function(_)
+            vim.lsp.config('*', {
+
+            })
+        end
+    },
+    {
+        "lazydev.nvim",
+        for_cat = "neonixdev",
+        cmd = { "Lazydev" },
+        ft = "lua",
+        after = function(_)
+            require('lazydev').setup({
+                library = {
+                    { words = { "nixCats" }, path = (nixCats.nixCatsPath or "") .. '/lua'},
+                },
+            })
+        end
+    },
+    {
+        "lua_ls",
+        enabled = nixCats('lua') or nixCats('neonixdev') or false,
+        lsp = {
+            filetypes = { "lua" },
+            settings = {
+                Lua = {
+                    runtime = { version = "LuaJIT" },
+                    formatters = {
+                        ignoreComments = true,
+                    },
+                    signatureHelp = { enabled = true },
+                    diagnostics = {
+                        globals = { "nixCats", "vim" },
+                        disable = { "missing-fields" },
+                    },
+                    telemetry = { enabled = false },
+                },
+            },
+        },
+    },
+    {
+        "nixd",
+        enabled = catUtils.isNixCats and (nixCats('nix') or nixCats('neonixdev')) or false,
+        lsp = {
+            filetypes = { 'nix' },
+            settings = {
+                nixd = {
+                    nixpkgs = {
+                        expr = nixCats.extra("nixdExtras.nixpkgs") or [[import <nixpkgs> {}]],
+                    },
+                    options = {
+                        nixos = {
+                            expr = nixCats.extra("nixdExtras.nixos_options")
+                        },
+                        ["home-manager"] = {
+                            expr = nixCats.extra("nixdExtras.home_manager_options")
+                        }
+                    },
+                    formatting = {
+                        command = { "nixfmt" }
+                    },
+                    diagnostic = {
+                        suppress = {
+                            "sema-escaping-with"
+                        }
+                    },
+                }, 
+            },
+        },
+    },
+}
