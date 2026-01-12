29f417e8f80dd61cbdeedd6eabd0054faa91423e
Dobin Hyeon <dobinhyeon@gmail.com>
2026-01-12 20:28:41 +0900

    add mini modules
    surround
    bufremove

--- lua/dbLuaConf/Plugins/mini.lua
+++ lua/dbLuaConf/Plugins/mini.lua
@@ -38,5 +38,41 @@ return {
 		nmap("<leader>e", function()
 			require("mini.files").open()
 		end, "Open mini.files [E]xplorer")
+
+		-- surround
+		require("mini.surround").setup({
+			mappings = {
+				add = "gsa",
+				delete = "gsd",
+				find = "gsf",
+				find_left = "gsF",
+				highlight = "gsh",
+				replace = "gsr",
+				update_n_lines = "gsn",
+
+				suffix_last = "l",
+				suffix_next = "n",
+			},
+		})
+
+		-- buf remove
+		require("mini.bufremove").setup()
+		nmap("<leader>bd", function()
+			local bd = require("mini.bufremove").delete
+			if vim.bo.modified then
+				local choice = vim.fn.confirm(("Save changes to %q?"):format(vim.fn.bufname()), "&Yes\n&No\n&Cancel")
+				if choice == 1 then
+					vim.cmd.write()
+					bd(0)
+				elseif choice == 2 then
+					bd(0, true)
+				end
+			else
+				bd(0)
+			end
+		end, "Delete Buffer")
+		nmap("<leader>Bd", function()
+			require("mini.bufremove").delete(0, true)
+		end, "Force delete buffer")
 	end,
 }
