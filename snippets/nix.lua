2c792a898a905607ed9e8ed0855ec4247c1a8879
hyeondobin <dobinhyeon@gmail.com>
2026-01-23 14:28:20 +0900

    add nix snippet for toggle module

--- snippets/nix.lua
+++ snippets/nix.lua
@@ -6,11 +6,38 @@ local t = ls.text_node
 local i = ls.insert_node
 local f = ls.function_node
 local parse = require("luasnip.util.parser").parse_snippet
+local fmt = require("luasnip.extras.fmt").fmta
 
 return {
 	s("custog", {
 		t("{ config, lib, ...}: {\n"),
-		i(1),
+		i(0),
 		t("}"),
 	}),
+	s(
+		"moduleTog",
+		fmt(
+			[[
+	{ config, lib, ...}:
+	let
+	  cfg = config.dbConfig;
+	in
+	{
+	  options = {
+	    dbConfig.<toggleName> = lib.mkEnableOption "Enable <toggleName>";
+	  };
+	  config = lib.mkIf cfg.<toggleName> {
+		<body>
+	  };
+	<>
+	}
+	]],
+			{
+				toggleName = i(1, "Name_of_toggle"),
+				body = i(2),
+				i(0),
+			},
+			{ repeat_duplicates = true }
+		)
+	),
 }
