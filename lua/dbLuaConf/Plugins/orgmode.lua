c28cdb7fa02764c3f74e2dd3e55c1fb1c65a1f57
hyeondobin <dobinhyeon@gmail.com>
2026-04-06 13:55:33 +0900

    edit orgmode config

--- lua/dbLuaConf/Plugins/orgmode.lua
+++ lua/dbLuaConf/Plugins/orgmode.lua
@@ -7,6 +7,18 @@ return {
 			require("orgmode").setup({
 				org_agenda_files = "~/Dropbox/org/**/*",
 				org_default_notes_file = "~/Dropbox/org/refile.org",
+				mappings = {
+					-- prefix = "<localleader>o",
+					org = {
+						org_edit_special = "<localleader>e",
+						org_babel_tangle = "<localleader>bt",
+					},
+					edit_src = {
+						org_edit_src_save_exit = "<localleader>s",
+						org_edit_src_abort = "<localleader>k",
+						org_edit_src_save = "<localleader>w",
+					},
+				},
 			})
 
 			-- Experimental LSP support
